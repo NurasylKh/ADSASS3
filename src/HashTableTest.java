9fe18df644719ebfbb18e53e957faab990949580
ASUS <231001@astanait.edu.kz>
2024-05-14 22:14:08 +0500

    HashTableTest

--- src/HashTableTest.java
+++ src/HashTableTest.java
@@ -2,7 +2,7 @@ public class HashTableTest {
     public static void main(String[] args) {
         MyHashTable<TestingField, Student> table = new MyHashTable<>();
 
-        // Inserting 10000 random elements
+        // Inserting 10000 random elements.
         for (int i = 0; i < 10000; i++) {
             TestingField key = new TestingField("Key" + (int)(Math.random() * 10000));
             Student value = new Student("Student" + i, (int)(Math.random() * 100));
